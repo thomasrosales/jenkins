710219a1f4a760366e3afa1820516569485b1bbe
Tomás Rosales <tomasjoaquinrosales@gmail.com>
2020-05-03 15:43:21 -0300

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,4 +1,4 @@
-job('NodeJS example') {
+job('NodeJS Docker example') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
